1df0b0b028ca8035f35b9f04b04bae781a30355e
naoto0822 <n.h.in.m.h@gmail.com>
2021-03-17 09:53:26 +0900

    fixed sample query

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,10 @@ use toy_rsql_parser::lexer::Lexer;
 use toy_rsql_parser::parser::Parser;
 
 fn main() {
-    let query = String::from("SELECT id FROM user;");
+    let query = String::from("SELECT id, name FROM user;");
     println!("query: {}", query);
     let lexer = Lexer::new(query);
     let mut parser = Parser::new(lexer);
     let stmts = parser.parse();
-    println!("ast: {:?}", stmts);
+    println!("ast:\n{:?}", stmts);
 }
